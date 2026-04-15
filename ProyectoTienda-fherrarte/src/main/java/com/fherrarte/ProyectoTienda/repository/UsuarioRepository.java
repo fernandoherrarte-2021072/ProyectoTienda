e64ad4fc2b845f544a416f7ee1bea59cd1aeb41b
fernandoherrarte-2021072 <fherrarte-2021072@kinal.edu.gt>
2026-04-14 23:59:38 -0600

    feat: Se agrego método de búsqueda por usename en UsuarioRepository

--- ProyectoTienda-fherrarte/src/main/java/com/fherrarte/ProyectoTienda/repository/UsuarioRepository.java
+++ ProyectoTienda-fherrarte/src/main/java/com/fherrarte/ProyectoTienda/repository/UsuarioRepository.java
@@ -4,6 +4,14 @@ import com.fherrarte.ProyectoTienda.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    // Para LOGIN
+    Optional<Usuario> findByUsernameAndPassword(String username, String password);
+
+    // Para REGISTRO (verifica si ya existe)
+    Optional<Usuario> findByUsername(String username);
 }
