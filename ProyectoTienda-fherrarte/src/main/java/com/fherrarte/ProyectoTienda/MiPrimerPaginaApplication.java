6abfc878db480a496303b0051536795472f23e04
Informatica <informatica@alumno.local>
2026-04-15 08:23:10 -0600

    style: Se ajusto estilo del avatar y la alineacion del header

--- ProyectoTienda-fherrarte/src/main/java/com/fherrarte/ProyectoTienda/MiPrimerPaginaApplication.java
+++ ProyectoTienda-fherrarte/src/main/java/com/fherrarte/ProyectoTienda/MiPrimerPaginaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class
-MiPrimerPaginaApplication implements CommandLineRunner {
+    MiPrimerPaginaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MiPrimerPaginaApplication.class, args);
