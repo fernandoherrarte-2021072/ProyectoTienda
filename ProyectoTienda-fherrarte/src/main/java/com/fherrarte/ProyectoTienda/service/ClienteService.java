76a8407c0efc8f97f2451071f668da8caef3e99b
fernandoherrarte-2021072 <fherrarte-2021072@kinal.edu.gt>
2026-04-15 01:52:53 -0600

    fix: Se cambio parámetros de DPI a String en ClienteService

--- ProyectoTienda-fherrarte/src/main/java/com/fherrarte/ProyectoTienda/service/ClienteService.java
+++ ProyectoTienda-fherrarte/src/main/java/com/fherrarte/ProyectoTienda/service/ClienteService.java
@@ -1,15 +1,12 @@
 package com.fherrarte.ProyectoTienda.service;
 
 import com.fherrarte.ProyectoTienda.entity.Cliente;
-
 import java.util.List;
 
 public interface ClienteService {
-
     List<Cliente> getAllClientes();
-    Cliente getClienteById(Integer dpi);
+    Cliente getClienteById(String dpi); // Cambiado a String
     Cliente saveCliente(Cliente cliente) throws RuntimeException;
-    Cliente updateCliente(Integer dpi, Cliente cliente);
-    void deleteCliente(Integer dpi);
-
+    Cliente updateCliente(String dpi, Cliente cliente); // Cambiado a String
+    void deleteCliente(String dpi); // Cambiado a String
 }
